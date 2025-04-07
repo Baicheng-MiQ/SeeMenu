06083b8f013174e581a75e48f7e4fdff00133536
Baicheng Yin <y@yinbaicheng.com>
2025-04-07 22:09:46 +0100

    Update .gitignore to include native directories, modify app.json to add bundle identifier and permissions, and update package.json and package-lock.json to include expo-dev-client and firebase dependencies.

---
+++ configs/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getVertexAI, getGenerativeAI } from 'firebase/generative-ai';
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBzmZCwRYBZDAAYpB-YIPXaB_YTJBYWJ7k",
+    authDomain: "seemenu-524e7.firebaseapp.com",
+    projectId: "seemenu-524e7",
+    storageBucket: "seemenu-524e7.firebasestorage.app",
+    messagingSenderId: "229273346393",
+    appId: "1:229273346393:web:6a900ff3b88f6e3ccc6c48",
+    measurementId: "G-P0LC7QNGKF"
+  };
+
+const app = initializeApp(firebaseConfig);
+const vertexAI = getVertexAI(app);
+
+export { app,vertexAI };
