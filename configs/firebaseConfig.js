f8a553b58b6701671a0f77a96ea2cd5d74b3ed0d
Baicheng Yin <y@yinbaicheng.com>
2025-04-08 18:14:30 +0100

    revoked exposed api key

--- configs/firebaseConfig.js
+++ configs/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getVertexAI } from 'firebase/vertexai';
 
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBzmZCwRYBZDAAYpB-YIPXaB_YTJBYWJ7k",
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
     authDomain: "seemenu-524e7.firebaseapp.com",
     projectId: "seemenu-524e7",
     storageBucket: "seemenu-524e7.firebasestorage.app",
     messagingSenderId: "229273346393",
-    appId: "1:229273346393:web:6a900ff3b88f6e3ccc6c48",
-    measurementId: "G-P0LC7QNGKF"
+    appId: "1:229273346393:web:8a33d3ecef77ffa2cc6c48",
+    measurementId: "G-ZY30DDS170"
   };
 
 const app = initializeApp(firebaseConfig);
